c493a0575ce55369977b09bf5ec687bfa7365564
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-07-18 20:54:08 -0300

    usando funcao com o objtos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,6 @@
 fun main() {
     println("Ola, bytebank")
-    val account = Account()
-    account.titular = "rodrigo vaz"
-    println(account.titular)
 
-    val accountMari = Account()
-    accountMari.titular = "Conta da mariana"
-    println(accountMari.titular)
 }
 
 class Account {
@@ -14,7 +8,10 @@ class Account {
     var numberAccount = 0
     var balance = 0.0
 
+}
 
+fun depositar(account: Account, value: Double) {
+    account.balance += value
 }
 
 fun testaCondicoes(balance: Double) {
@@ -61,4 +58,36 @@ fun testeVariaveis() {
     println("titular da conta $holder")
     println("numero de conta: $accountNumber")
     println("saldo da conta: $balance")
+}
+
+fun testeOrientacaoAObjecto() {
+    val account = Account()
+    account.titular = "rodrigo vaz"
+    println(account.titular)
+
+    val accountMari = Account()
+    accountMari.titular = "Conta da mariana"
+    accountMari.numberAccount = 1001
+    accountMari.balance = 1000.00
+    println(accountMari.titular)
+    println(accountMari.numberAccount)
+    println(accountMari.balance)
+
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+    println("O numero X $numeroX")
+    println("o numero Y $numeroY")
+
+
+    val accountJoao = Account()
+    accountJoao.titular = "Joao"
+    var accountMaria = accountJoao
+    accountMaria.titular = "Maria"
+
+    println("A titula da conta joao ${accountJoao.titular}")
+    println("O titular da conta maria e ${accountMaria.titular}")
+
+    depositar(accountJoao, 250.00)
+    depositar(accountMaria, 25079.00)
 }
