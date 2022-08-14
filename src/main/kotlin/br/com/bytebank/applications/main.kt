bce796f4b6813394cb7e4f146273d36c325c9cb8
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-08-14 11:20:18 -0300

    testando o let para verificar safe null

--- src/main/kotlin/br/com/bytebank/applications/main.kt
+++ src/main/kotlin/br/com/bytebank/applications/main.kt
@@ -3,8 +3,15 @@ package br.com.bytebank.applications
 import br.com.bytebank.models.accounts.Address
 
 fun main() {
-    val addressNull:Address? = null
-    val addressNotNull: Address = addressNull!!
-    addressNotNull.city
+    val addressNull:Address? = Address(city = "Uberlandia",
+        complement = "43",
+        neighborhood = "asdfads",
+        number = 3, publicPlace = "43324",
+        state = "43232",
+        zipCode = "354234")
 
+    addressNull?.let { address ->
+        println(address.city.length)
+    }
+    println(addressNull?.city)
 }
