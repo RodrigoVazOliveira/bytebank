58dc711bdf155f653c3048a00e286d1c69bdd87b
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-08-14 09:29:34 -0300

    pegando excecao aleatoria

--- src/main/kotlin/br/com/bytebank/applications/testeAccounts.kt
+++ src/main/kotlin/br/com/bytebank/applications/testeAccounts.kt
@@ -1,6 +1,8 @@
 import br.com.bytebank.models.Client
 import br.com.bytebank.models.accounts.AccountChain
 import br.com.bytebank.models.accounts.AccountSaving
+import br.com.bytebank.models.exceptions.InsufficientFundsException
+import br.com.bytebank.models.exceptions.UnauthenticationException
 import java.util.UUID
 
 fun main() {
@@ -18,7 +20,20 @@ fun main() {
     contaCorrente.depositar(28000.00)
     contaCorrente.sacar(25.00)
     println("o valor do balanco e ${contaCorrente.balance}")
-    contaCorrente.transfer(accountSaving, 200.00, "12312312121")
+
+    try {
+        contaCorrente.transfer(accountSaving, 200.00, "12312312121")
+    } catch (e: InsufficientFundsException) {
+        println(e.message)
+        e.printStackTrace()
+    } catch (e: UnauthenticationException) {
+        println(e.message)
+        e.printStackTrace()
+    } catch (e: Exception) {
+        println(e.message)
+        e.printStackTrace()
+    }
+
     accountSaving.depositar(2500000.00)
     accountSaving.sacar(200.00)
     accountSaving.transfer(contaCorrente, 2500.00, "123123121231")
