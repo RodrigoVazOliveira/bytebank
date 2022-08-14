f07de9a80bde58f9d78b5210b36e70cfdfc9134a
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-08-14 07:56:01 -0300

    try como expressiones

--- src/main/kotlin/br/com/bytebank/applications/main.kt
+++ src/main/kotlin/br/com/bytebank/applications/main.kt
@@ -6,18 +6,54 @@ fun main() {
     println("início main")
     functionOne()
     println("fim main")
+
+    val entrada: String = "1.9"
+        val value: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Problema ao converter")
+        e.printStackTrace()
+        null
+    }
+
+    val valurWithTax: Double? = if (value != null) {
+        value + (value * 0.1)
+    } else {
+        null
+    }
+
+    if (value != null) {
+        println("valor recebido: $value")
+    } else {
+        println("Valor invalido")
+    }
+
+    println("valor com taxa: $valurWithTax")
 }
 
 fun functionOne() {
     println("início funcao1")
     functionTwo()
     println("fim funcao1")
+
+
+
 }
 
 fun functionTwo() {
-    val divisor = 1 / 0
-    val adress = Any()
-    adress as Address
+
+    try {
+        val divisor = 1 / 0
+    } catch (e: ArithmeticException) {
+        println(e.message)
+    }
+
+    try {
+        val adress = Any()
+        adress as Address
+    } catch (e: ClassCastException) {
+        println(e.message)
+    }
 
 
 
@@ -26,4 +62,7 @@ fun functionTwo() {
         println(i)
     }
     println("fim funcao2")
+
+
+
 }
